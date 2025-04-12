89ceb1b9fedd01741449adb028bc67262f1e36de
Jacob Sorensen <sorensenjacob44@gmail.com>
2025-04-11 20:18:59 -0400

    Add timestep analysis to test1

--- tests/test1.py
+++ tests/test1.py
@@ -2,7 +2,7 @@ import rusty_orbital_dynamics as rod
 import matplotlib.pyplot as plt
 import numpy as np 
 
-def Basic_Graphic(data,size):
+def Basic_Graphic(data,size,filename):
     y_values = np.array(data[1])
     # Static Plot
     # plt.clf()
@@ -23,8 +23,7 @@ def Basic_Graphic(data,size):
     plt.legend()
     plt.grid()
     plt.axis('equal')
-    plt.show()
-    plt.savefig('test1.png')
+    plt.savefig(filename + ".png")
 
 def Formatter(vector):
     VN = []
@@ -52,8 +51,21 @@ vectors = [
     [0,6000000,0],
     [50,0,0]
 ]
+print("running test...")
+data = rod.rkf45(Formatter(vectors),masses,0,150000,1,6.67259*10**-20,10**-10,10**5,10**-5)
 
-data = rod.rkf45(Formatter(vectors),masses,0,150000,1,6.67259*10**-20,10**-10,10,0.1)
-
+print("plotting results...")
 # Generates a static plot of the data
-Basic_Graphic(data,len(masses))
+Basic_Graphic(data,len(masses),"test1_orbits")
+
+# print(data[0])
+lhs = np.array(data[0][0:(len(data[0])-2)])
+rhs = np.array(data[0][1:(len(data[0])-1)])
+
+timesteps = rhs - lhs
+
+plt.figure( figsize=(5,5) )
+plt.plot( lhs , timesteps , label = 'dt over time'  )
+plt.legend()
+plt.grid()
+plt.savefig("test1_timestep_change.png")
